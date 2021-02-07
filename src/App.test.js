0dc7fa862be4d20c81fd2b263118bb9fb537ebc2
Dmitry Fedyuk <dfediuk@gmail.com>
2021-02-07 20:20:46 +0600

    https://reactjs.org/docs/create-a-new-react-app.html#create-react-app

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,8 @@
-import { render, screen } from '@testing-library/react';
+// 2021-02-07 https://reactjs.org/docs/create-a-new-react-app.html#create-react-app
+import {render, screen} from '@testing-library/react';
 import App from './App';
-
 test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+	render(<App/>);
+	const linkElement = screen.getByText(/learn react/i);
+	expect(linkElement).toBeInTheDocument();
 });
