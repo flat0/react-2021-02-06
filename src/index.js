c4d4d7f088ec702f99a7a849cb387dafc574ec09
Dmitry Fedyuk <dfediuk@gmail.com>
2021-02-08 05:31:15 +0600

    https://reactjs.org/tutorial/tutorial.html#why-immutability-is-important

--- src/index.js
+++ src/index.js
@@ -17,6 +17,19 @@ class Board extends React.Component {
 	 * @param {Number} i
 	 */
 	handleClick(i) {
+		// 2021-02-08
+		// 1) «Avoiding direct data mutation lets us keep previous versions of the game’s history intact, and reuse them later»:
+		// https://reactjs.org/tutorial/tutorial.html#complex-features-become-simple
+		// 2) «Detecting changes in mutable objects is difficult because they are modified directly.
+		// This detection requires the mutable object to be compared to previous copies of itself
+		// and the entire object tree to be traversed.
+		// Detecting changes in immutable objects is considerably easier.
+		// If the immutable object that is being referenced is different than the previous one, then the object has changed.»
+		// https://reactjs.org/tutorial/tutorial.html#detecting-changes
+		// 3) «The main benefit of immutability is that it helps you build pure components in React.
+		// Immutable data can easily determine if changes have been made,
+		// which helps to determine when a component requires re-rendering.»
+		// https://reactjs.org/tutorial/tutorial.html#determining-when-to-re-render-in-react
 		const squares = this.state.squares.slice();
 		squares[i] = 'X';
 		this.setState({squares: squares});
