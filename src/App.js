6a79d600a4560992ffba47627c8e09c01443d9c5
Dmitry Fedyuk <dfediuk@gmail.com>
2021-02-07 20:19:51 +0600

    no message

--- src/App.js
+++ src/App.js
@@ -3,15 +3,15 @@ import logo from './logo.svg';
 import './App.css';
 function App() {
 	return (
-		<div className="App">
-			<header className="App-header">
-				<img src={logo} className="App-logo" alt="logo"/>
+		<div className='App'>
+			<header className='App-header'>
+				<img src={logo} className='App-logo' alt='logo'/>
 				<p>Edit <code>src/App.js</code> and save to reload 111.</p>
 				<a
-					className="App-link"
-					href="https://reactjs.org"
-					rel="noopener noreferrer"
-					target="_blank"
+					className='App-link'
+					href='https://reactjs.org'
+					rel='noopener noreferrer'
+					target='_blank'
 				>Learn React</a>
 			</header>
 		</div>
